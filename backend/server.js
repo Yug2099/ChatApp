1e853e1717cb812375b88667b86127cb9e2e5c02
Yug2099 <yug.g03@gmail.com>
2024-07-08 08:15:52 +0530

    Native web socket

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,8 @@ const PORT = process.env.PORT || 5000; // Set a default port if PORT environment
 
 const server = app.listen(PORT, console.log(`Server started at port ${PORT}`));
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
+// const WebSocket = require("ws");
+const wss = new WebSocket({ server });
 
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
